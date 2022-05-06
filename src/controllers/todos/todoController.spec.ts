67d57446606f468631768abf0de7f2a3b7f3ff18
dadadorly <david.nicola.luu@gmail.com>
2022-05-06 15:58:42 +0200

    ci: TodoController's unit tests

--- src/controllers/todos/todoController.spec.ts
+++ src/controllers/todos/todoController.spec.ts
@@ -1,55 +1,110 @@
-/*
-import todoModel from '../../models/todoModel';
-import { Request, Response } from 'express';
-import { TodoController } from './todoController';
+import { TodoController } from "./todoController";
 
-jest.mock('../../models/todoModel');
+jest.mock("./todoService");
 
-export interface Todo {
-  id: number | string;
-  task: string;
-  state: boolean;
-}
-
-describe('todoController', () => {
-  beforeEach(() => {
-    (todoModel.findById as jest.Mock).mockClear();
-  });
-  describe('getTodos()', () => {
-    it('should return empty array', async () => {
+describe("todoController", () => {
+  describe("getTodos()", () => {
+    it("should return empty array", async () => {
       // given
       const todoController = new TodoController();
-      (todoModel.find as jest.Mock).mockResolvedValue([]);
-      const req = {} as any;
-      const res = { send: jest.fn() } as any;
+
+      // @ts-ignore
+      (todoController.todoService.getTodos as jest.Mock).mockResolvedValue([]);
       // when
-      const result = await todoController.getTodos(req, res);
+      const result = await todoController.getTodos();
       // then
       expect(result).toEqual([]);
     });
   });
-
-  describe('getTodo()', () => {
-    it('should return a todo from todoModel', async () => {
+  describe("getTodo()", () => {
+    it("should return a todo from todoModel", async () => {
       // given
       const todoController = new TodoController();
-      (todoModel.findById as jest.Mock).mockResolvedValue([
+      // @ts-ignore
+      (todoController.todoService.getTodo as jest.Mock).mockResolvedValue([
         {
-          task: 'I am a task'
+          task: "I am a task"
         }
       ]);
-      const req = { params: { id: 'id' } } as any;
+      const req = { params: { id: "id" } } as any;
       // when
       const result = await todoController.getTodo(req);
       // then
       expect(result).toEqual([
         {
-          task: 'I am a task'
+          task: "I am a task"
         }
       ]);
-      expect(todoModel.findById as jest.Mock).toHaveBeenCalledWith('id');
+      // @ts-ignore
+      expect(todoController.todoService.getTodo as jest.Mock).toHaveBeenCalledWith("id");
     });
-
+  });
+  describe("addTodo()", () => {
+    it("should add a new todo in database", async () => {
+      // given
+      const todoController = new TodoController();
+      //@ts-ignore
+      (todoController.todoService.addTodo as jest.Mock).mockResolvedValue({
+        id: "id",
+        task: "task",
+        done: false
+      });
+      const req = {
+        body: {
+          task: "task"
+        }
+      } as any;
+      // when
+      const result = await todoController.addTodo(req);
+      // then
+      expect(result).toEqual({
+        id: "id",
+        task: "task",
+        done: false
+      });
+    });
+  });
+  describe("deleteTodo()", () => {
+    it("should delete a todo by id", async () => {
+      // given
+      const todoController = new TodoController();
+      //@ts-ignore
+      (todoController.todoService.deleteTodo as jest.Mock).mockResolvedValue([]);
+      const req = { params: { id: "1A" } } as any;
+      // when
+      const result = await todoController.deleteTodo(req);
+      // then
+      expect(result).toEqual([]);
+    });
+  });
+  describe("updateTodo()", () => {
+    it("should update a todo", async () => {
+      // given
+      const todoController = new TodoController();
+      //@ts-ignore
+      (todoController.todoService.updateTodo as jest.Mock).mockResolvedValue({
+        id: "some-id",
+        task: "task",
+        done: false
+      });
+      const req = { params: { id: "some-id" }, body: { task: "task", done: true } } as any;
+      // when
+      const result = await todoController.updateTodo(req);
+      // then
+      expect(result).toEqual({
+        id: "some-id",
+        task: "task",
+        done: false
+      });
+    });
+  });
+});
+/*
+describe('todoController', () => {
+  beforeEach(() => {
+    (todoModel.findById as jest.Mock).mockClear();
+  });
+  describe('getTodo()', () => {
     it('should throw todo error not found', async () => {
       // given
       const todoController = new TodoController();
@@ -61,56 +116,7 @@ describe('todoController', () => {
       expect(result).toEqual('todo not found');
     });
   });
-  describe('addTodo()', () => {
-    it('should add a new todo in database', async () => {
-      // given
-      const todoController = new TodoController();
-      const save = (jest.fn() as jest.Mock).mockResolvedValue({
-        id: "id",
-        task: "task",
-        done: false
-      });
-      (todoModel as jest.MockedFunction<any>).mockImplementation(() => {
-        return {
-          save
-        };
-      });
-      const req ={
-        body: {
-          task: "task",
-          done: false
-        }
-      } as any
-      // when
-        const result = await todoController.addTodo(req)
-      // then
-      expect(todoModel).toHaveBeenCalledWith({
-        task: "task",
-        done: false
-      });
-      expect(save).toHaveBeenCalled();
-      expect(save).toHaveReturned();
-      expect(result).toEqual({
-        id: "id",
-        task: "task",
-        done: false
-      });
-    });
-  });
   describe('deleteTodo()', () => {
-    it('should delete a todo by id', async () => {
-      // given
-
-      (todoModel.findByIdAndDelete as jest.Mock).mockResolvedValue([]);
-      const req = { params: { id: '1A' } } as any;
-
-
-      // when
-      const todoController = new TodoController()
-      const result = await todoController.deleteTodo(req);
-      // then
-      expect(result).toEqual([]);
-    });
     it('should return todo not found ', async () => {
       // given
 
